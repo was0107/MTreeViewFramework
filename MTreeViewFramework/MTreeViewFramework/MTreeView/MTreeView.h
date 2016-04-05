bd2508d4a696207c4a8aca701b50188336d8b0e9
boguang <boguang@b5m.com>
2016-04-05 18:17:08 +0800

    update readme.md

--- MTreeViewFramework/MTreeViewFramework/MTreeView/MTreeView.h
+++ MTreeViewFramework/MTreeViewFramework/MTreeView/MTreeView.h
@@ -14,16 +14,54 @@
 
 @interface MTreeView : UITableView
 
-@property (nonatomic, readwrite, strong) MTreeNode *rootNode;
+@property (nonatomic, readwrite, strong) MTreeNode *rootNode;           //根节点
 
 @property (nonatomic, readwrite, weak) IBOutlet id<MTreeViewDelegate> treeViewDelegate;
 
+
+/**
+ *  返回根节点的子节点数，即深度为0的节点数
+ *
+ *  @parames
+ *  @param  treeView
+ *
+ *  @return 根节点的子节点数
+ *
+ */
 - (NSInteger) numberOfSectionsInTreeView:(MTreeView *)treeView;
 
+/**
+ *  返回深度为0的所有子孙节点数
+ *
+ *  @parames
+ *  @param  treeView
+ *  @param  section
+ *
+ *  @return 深度为0的所有子孙节点数
+ *
+ */
 - (NSInteger) treeView:(MTreeView *)treeView numberOfRowsInSection:(NSInteger)section;
 
+/**
+ *  展开或者折叠当前节点
+ *
+ *  @parames
+ *  @param  indexPath   当row为负数时，则表示展开一级节点，即对应的Section点击事件处理
+ *
+ *  @return
+ *
+ */
 - (void) expandNodeAtIndexPath:(NSIndexPath *)indexPath;
 
+/**
+ *  获取指定path对应的节点
+ *
+ *  @parames
+ *  @param  indexPath   当row为负数时，则表示获取一级节点
+ *
+ *  @return 对应的节点
+ *
+ */
 - (MTreeNode *) nodeAtIndexPath:(NSIndexPath *)indexPath;
 
 @end
